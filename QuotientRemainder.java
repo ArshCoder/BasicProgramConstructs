8f97bf25547d1e7a35558ea4a4a78ac3655ea088
CoderArsh <arshjeetb08@gmail.com>
2021-09-28 19:53:18 +0530

    Added: For Factor

--- QuotientRemainder.java
+++ QuotientRemainder.java
@@ -1,5 +1,7 @@
 package constructs;
 
+import java.util.Scanner;
+
 public class QuotientRemainder {
 
 	public static void main(String[] args) {
@@ -11,5 +13,25 @@ public class QuotientRemainder {
 
 		System.out.println("Quotient = " + quotient);
 		System.out.println("Remainder = " + remainder);
+	}
+	public static void findFactor(int n) {
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0)
+				System.out.print(i + "\t");
+		}
+	}
+
+	public static void main1(String[] args) {
+		int number = 0;
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Enter a number:: ");
+		number = scan.nextInt();
+
+		System.out.print("The factors are:: ");
+		findFactor(number);
+
+		scan.close();
+
 	}
 }
